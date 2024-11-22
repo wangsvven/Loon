e955202e35d862872d4dc8099caa796d50eebce3
wangsvven <134424041+wangsvven@users.noreply.github.com>
2024-11-22 17:18:09 +0800

    Update AppRaven.js

--- Scripts/AppRaven.js
+++ Scripts/AppRaven.js
@@ -2,7 +2,7 @@
 AppRaven
 
 [rewrite_local]
-https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/AppRaven.js
+https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/wangsvven/Loon/refs/heads/main/Scripts/AppRaven.js
 
 [mitm] 
 hostname = appraven.net
